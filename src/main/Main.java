38dd1a09646d5d9615ee3ccfcafe56c5fc0738df
Baselgeuse <97988083+Baselgeuse@users.noreply.github.com>
2022-03-21 17:16:12 -0400

    Program re-write
    Re-wrote program to get rid of array and check for spaces

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -33,55 +32,37 @@ public class Main {
 	@SuppressWarnings("resource")
 	public static void convertCSVtoHTML(File f) throws CSVAttributeMissing, CSVDataMissing {
 		
-		BufferedReader br = null;
 		Scanner sc = null;
 		PrintWriter pw = null;
-		int count = 0;
+		int row = 1;
 		
 		try {
-			br = new BufferedReader(new FileReader(f));
 			sc = new Scanner(new FileInputStream(f)).useDelimiter(",");
 			
-			while(br.readLine() != null) {
+			System.out.print(sc.nextLine());
+			
+			int count = 0;
+			while(sc.hasNextLine()) {
+				String token = sc.next();
+				System.out.print(token + " ");
 				count++;
+				if(count == 4) {
+					System.out.print(row);
+					row++;
+					count = 0;
+				}
+				if(row == 1) {
+					if(token == " ")
+						throw new CSVAttributeMissing("ERROR: In file " + f.getName() + ". Missing attribute. File not converted to HTML.");
+				}
+				if(row != 1 && token == " ")
+					throw new CSVDataMissing("ERROR: In file " + f.getName() + ". Line " + row + " is missing a value. File not converted to HTML.");
+				sc.close();
 			}
-			br.close();
-			}
-		catch(Exception e) {
+		}
+		catch(FileNotFoundException e) {
 			System.out.print("File not found. Terminating program.");
 			System.exit(0);
-		}
-		
-		String[][] array = new String[count][4];
-		
-		array[0][0] = sc.next();
-		
-		System.out.print(array[0][0]);
-		
-		for(int i  = 1; i < count; i++) {
-				for(int j  = 0; j < 4; j++) {
-					String value  = sc.next();
-					if(value.equals("Note:")) {
-						array[count-1][0] = value;
-					}
-					array[i][j] = value;
-					System.out.print(array[i][j] + " ");
-			}	
-		}
-		sc.close();
-	
-		try {
-			for(int k = 0; k < array.length; k++) {
-				for(int l = 0; l < array[k].length; l++) {
-					if(array[1][l] == " ") {
-						throw new CSVAttributeMissing("ERROR: In file " + f.getName() + ". Missing attribute. File not converted to HTML.");
-					}
-					if(k != 1 && array[k][l] == " ") {
-						throw new CSVDataMissing("ERROR: In file " + f.getName() + ". Line " + k + " is missing a value. File not converted to HTML."
-								+ " Missing data in " + (String) array[1][k] + ".");
-					}
-				}
-			}
 		}
 		catch(CSVAttributeMissing a) {
 			
@@ -109,61 +90,15 @@ public class Main {
 			pw.println(d.getMessage());
 			pw.close();
 		}
+	
 		
-		PrintWriter output;
-				
-		try {
-			output = new PrintWriter("C:\\Users\\liamd\\Documents\\GitHub\\CSV2HTML\\src\\generated-html-files\\" + f.getName() + ".html");
-			output.println("<!DOCTYPE HTML>");
-			output.println("<html>");
-			output.println("<head><title> HTML Tables </title></head>");
-			output.println("<body>");
-			output.println("<table bgcolor = \"black\" align = \"center\" width = \"700\">");
-			
-			boolean titleWritten = false;
-			String noteTest = array[array.length - 1][0].substring(0,6);
-			boolean hasNote = noteTest.equals("Note:");
-			
-			for(int i = 0; i < array.length; i++) {
-				
-				output.println("<tr bgcolor = \"grey\" align = \"center\" width = \"200\">");
-				
-				for(int j = 0; j < array[i].length; j++) {
-					
-					if(i == 0 && titleWritten == false) {
-						output.println("<caption>" + array[i][j] + "</caption>");
-						titleWritten = true;
-					}
-					
-					else if(i == 0 || array[i][j].equals(null))
-						continue;
-					
-					else if(i == 1) {
-						output.println("<th>" + array[i][j] + "</th>");
-					}
-					
-					else if(i == array.length && hasNote == true)
-						output.println("<span>" + array[i][j] + "</span>");
-					
-					else {
-						output.println("<td>" + array[i][j] + "</td>");
-					}
-					
-				}
-				output.println("</tr>");
-			}
-			
-			output.println("</table>");
-			output.println("</body>");
-			output.println("</html>");
-			
-			output.close();
-			
-			System.out.println("Program gets to the end without any errors!");
-			
-		} catch (FileNotFoundException e) {
-			System.out.println(e.getMessage());
-		}
+		
+		
+		
+		
+		
+	
+		
 		
 	}
 
